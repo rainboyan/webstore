c2932354189a6d49e55150db9ebdbc4ef60f0965
rainboyan <rainboyan@gmail.com>
2014-06-27 16:59:55 +0800

    Upgrade grails to 2.4.2 and gradle plugin to 2.1.0

--- webstore/grails-app/conf/BuildConfig.groovy
+++ webstore/grails-app/conf/BuildConfig.groovy
@@ -58,8 +58,8 @@ grails.project.dependency.resolution = {
         build ":tomcat:7.0.54"
 
         // plugins for the compile step
-        compile ":scaffolding:2.1.1"
-        compile ':cache:1.1.6'
+        compile ":scaffolding:2.1.2"
+        compile ':cache:1.1.7'
         compile ":asset-pipeline:1.8.11"
 
         // plugins needed at runtime but not for compilation
